85352b073f596e461de1603a90ace003ce4301bd
Harrison Ferrone <hferrone@gmail.com>
2017-04-21 21:00:50 +0200

    feat: Create Django App
    Initialized a Django app named blog to use in the project. Also created a post model class with some basic properties and a meta data subclass.

---
+++ blog/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    summary = models.CharField(max_length=300)
+    content = models.TextField()
+    published = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    # Sub-class
+
+    class Meta:
+        ordering = ['-created']
+
+        def __unicode__(self):
+            return u'%s' % self.title
+
+
+        def get_absolute_url(self):
+            return reverse('blog.views.post', args=[self.slug])
+
+
