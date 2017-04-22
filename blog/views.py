6e2a85d64e936404ad41235b85c67d8c4015178b
Harrison Ferrone <hferrone@gmail.com>
2017-04-22 19:06:40 +0200

    feat: Add Page Templates
    Created simple HTML page template for index route view.

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hey there.")
+    return render(request, 'index.html', {})
 
 
 def post(request):
